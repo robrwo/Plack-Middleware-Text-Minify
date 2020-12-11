d6c9bc507dab50f681d26e32a176a990f334128f
Robert Rothenberg <rrwo@cpan.org>
2020-12-11 11:51:44 +0000

    Add type attribute

--- lib/Plack/Middleware/Text/Minify.pm
+++ lib/Plack/Middleware/Text/Minify.pm
@@ -8,7 +8,7 @@ use warnings;
 use parent qw/ Plack::Middleware /;
 
 use Plack::Util;
-use Plack::Util::Accessor qw/ path /;
+use Plack::Util::Accessor qw/ path type /;
 use Ref::Util qw/ is_arrayref is_coderef /;
 use Text::Minify::XS v0.3.1 ();
 
@@ -38,7 +38,14 @@ sub call {
             return unless is_arrayref($res);
 
             my $type = Plack::Util::header_get( $res->[1], 'content-type' );
-            return unless $type =~ m{^text/};
+            if ( my $match = $self->type ) {
+                return
+                  unless ( ( is_coderef($match) && $match->( $type, $res ) )
+                    || ( $type =~ $match ) );
+            }
+            else {
+                return unless $type =~ m{^text/};
+            }
 
             my $body = $res->[2];
             return unless is_arrayref($body);
